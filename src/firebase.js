3461b999b532e1eaf1ec287526aee8ec91286441
feimpraim <fredickimpraim@gmail.com>
2020-09-08 16:17:34 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCmzopJOcRTcImIOngQZW_9ZgKctsnrYc",
+  authDomain: "app-3d8d1.firebaseapp.com",
+  databaseURL: "https://app-3d8d1.firebaseio.com",
+  projectId: "app-3d8d1",
+  storageBucket: "app-3d8d1.appspot.com",
+  messagingSenderId: "317339054780",
+  appId: "1:317339054780:web:83578c6dc536ac0719d6b9",
+};
